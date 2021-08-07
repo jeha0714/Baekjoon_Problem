34277c598737e607453d09c2baf2f29175269fdd
jeha0714 <74492789+jeha0714@users.noreply.github.com>
2021-08-07 19:28:55 +0900

    백준 11650번 문항
    <문제>
    2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
    
    <입력>
    첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
    
    <출력>
    첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.

---
+++ Implement/Silver 5/20210807_11650.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct //좌표 구조체 정의.
+{
+    int x_Location;
+    int y_Location;
+    
+}Location;
+
+
+int compare(const void *first, const void *second) { //오름차순 정의.
+    
+    if ( ((Location *)first)->x_Location == ((Location *)second)->x_Location ){
+        
+        return ((Location *)first)->y_Location - ((Location *)second)->y_Location;
+    }
+    else {
+        
+        return ((Location *)first)->x_Location - ((Location *)second)->x_Location;
+    }
+    
+    //return 0;
+}
+
+
+
+int main() {
+    
+    int N = 0; //점의 개수 N
+    scanf("%d", &N);
+    Location x_And_y[N]; //구조체 포인터 N 정의
+    
+    for (int i = 0; i < N; i++) { //정의된 구조체 타입의 각 배열에 좌표 저장.
+        
+        scanf("%d %d", &x_And_y[i].x_Location, &x_And_y[i].y_Location); 
+    }
+    
+    
+    qsort(x_And_y, N, sizeof(x_And_y[0]), compare); //qsort 진행.
+    
+    
+    for (int i = 0 ; i < N; i++) { //차례대로 구조체 내 변수를 통해 좌표 출력.
+        
+        printf("%d %d\n", x_And_y[i].x_Location, x_And_y[i].y_Location);
+    }
+    
+    
+    
+    return 0;
+}
